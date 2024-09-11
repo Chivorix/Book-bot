45574339257af40caccae7de36b3712c4591e320
Chivorix <danilocim98@gmail.com>
2024-09-11 20:56:16 +0200

    project finished

--- main.py
+++ main.py
@@ -3,12 +3,21 @@ frankenstein = "books/frankenstein.txt"
 def main(path_to_book): # extract the text as string
     with open(path_to_book) as f:
         file_contents = f.read()
-    return file_contents
+    text = file_contents
+    list_of_words,word_count = word_counter(text)
+    char_dict = character_counter(list_of_words)
+    list_of_dict = [{"letter" : l, "count": n} for l,n in char_dict.items()]
+    list_of_dict.sort(reverse=True, key=lambda x : x["count"])
+
+    print(f"--- Begin report of books/frankenstein.txt ---\n{word_count} words found in the document\n")
+    for d in list_of_dict:
+        print(f"The '{d['letter']}' character was found {d['count']} times")
+    print("--- End report ---")
 
 def word_counter(text): # Count the words
     words = text.split()
-    print(len(words))
-    return words
+    word_count = len(words)
+    return words,word_count
 
 def character_counter(word_list): # count of all the symbols in the text    
     char_count = {}
@@ -18,10 +27,7 @@ def character_counter(word_list): # count of all the symbols in the text
             char_count[letter] += 1
         elif letter.isalpha():
             char_count[letter] = 1
-    print(char_count)
     return char_count
+    
+main(frankenstein)
 
-
-text = main(frankenstein)
-list_of_words = word_counter(text)
-character_counter(list_of_words)
